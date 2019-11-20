54d0197a570ac4278a858148867176e58e3c3e51
WangZhen <294814110@qq.com>
2019-11-20 12:32:55 +0800

    Create Monitor.swift
    add

---
+++ CAVideo/Monitor.swift
@@ -0,0 +1,18 @@
+class Monitor {
+
+var n：Int
+var e：Double
+
+init(n:Int,e:Double) {
+
+	self.n = n
+	self.e = e
+}
+	
+func checkException(v:Int) ->Bool {
+  if n*v >=e {
+     return false;
+   }
+  return true;
+}	
+}
